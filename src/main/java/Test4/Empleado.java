9db6aa74e97cb921ffd0e83de567b6838c5a2403
Leandro Damo <leandrodamo1@gmail.com>
2019-08-14 13:05:31 -0300

    Subo tp nuevo

---
+++ src/main/java/Test4/Empleado.java
@@ -0,0 +1,13 @@
+package Test4;
+
+public class Empleado {
+    String nombre;
+    String apellido;
+    int dni;
+    String direccion;
+
+    int antiguedad;
+    String telefono;
+    double sueldo;
+
+}
